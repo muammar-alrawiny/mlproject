c6d30dc2c36fe8ae876397b3f172c57ca700dd3b
muammar-alrawiny <muammaralrawiny@gmail.com>
2023-05-30 18:07:42 +0200

    Data Transformation

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 from setuptools import find_packages,setup
+from typing import List
+
+HYFEN_E_DOT ='-e .'
+def get_requirments(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirment
+    
+    '''
+    requirments=[]
+    with open(file_path)as file_obj:
+        requirments=file_obj.readlines(),
+        requirments=[req.replace("\n","")for req in requirments]
+        if HYFEN_E_DOT in requirments:
+            requirments.remove(HYFEN_E_DOT)
+    return requirments        
 
 setup(
     name='mlproject',
@@ -6,5 +21,5 @@ setup(
     author='Muammar Alrawiny',
     author_email='muammaralrawiny@gmail.com',
     packages= find_packages(),
-    install_requirers = ['pandas','matplotlib','numpy','seaborn']
+    install_requirers = get_requirments('requirments.txt'),
 )
